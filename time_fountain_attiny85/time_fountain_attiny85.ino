f9f107de60e9d965f43eefbaee1bbb1867a8c02a
isaac879 <isaac879@live.com>
2020-01-10 18:55:43 +0000

    Add files via upload

---
+++ time_fountain_attiny85/time_fountain_attiny85.ino
@@ -0,0 +1,114 @@
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------
+ * CHECK THE CODE FOR "TODO:" AND EDIT APPROPRIATELY 
+ * 
+ * The code is developed for a RGB Time Fountain. The RGB Time Fountain is controlled by an Arduino Nano, ATtiny85, JDY-30 Bluetooth module (HC-05 and other serial pass-through modules should also be compatable but please check before using them.) and custom circuits. 
+ * 
+ * CAD files available at: Not currently publicly available.
+ * 
+ * All measurements are in SI units unless otherwise specified
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE
+ * 
+ * Code written by isaac879
+ * 
+ * Last modified 10/01/2020
+ *--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//interupt drop counter
+//pid
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#include <TinyWireS.h>
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+#ifndef TWI_RX_BUFFER_SIZE
+#define TWI_RX_BUFFER_SIZE (16)
+#endif
+
+#define I2C_ADDRESS_ATTINY85 0x25 //0b00100101 (Decimal 37)  
+#define PUMP_PIN PB1 //Physical pin 6
+#define PHOTOTRANSISTOR_PIN PB4 //Physical pin 3
+#define SDA_PIN PB0 //Physical pin 5
+#define SCL_PIN PB2 //Physical pin 7
+
+volatile uint16_t pwm = 0;
+volatile uint16_t set_point_frequency = 0;
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void receiveEvent(uint8_t howMany){
+    if (howMany < 1) return;// Sanity-check
+    if (howMany > TWI_RX_BUFFER_SIZE) return;// Also insane number
+    if (!howMany) return;// This write was only to set the buffer for next read
+
+    String pwmStr = "";
+    while(howMany--){
+        char c = TinyWireS.receive();
+        pwmStr += c;
+    }
+    pwm = pwmStr.toFloat();
+    //set_point_frequency = pwm;
+    //TODO: validate input
+    analogWrite(PUMP_PIN, pwm); 
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void setup(){
+    pinMode(PUMP_PIN, OUTPUT);
+//    Control Register A for Timer/Counter-0 (Timer/Counter-0 is configured using two registers: A and B)
+//    TCCR0A is 8 bits: [COM0A1:COM0A0:COM0B1:COM0B0:unused:unused:WGM01:WGM00]
+//    2<<COM0A0: sets bits COM0A0 and COM0A1, which (in Fast PWM mode) clears OC0A on compare-match, and sets OC0A at BOTTOM
+//    2<<COM0B0: sets bits COM0B0 and COM0B1, which (in Fast PWM mode) clears OC0B on compare-match, and sets OC0B at BOTTOM
+//    3<<WGM00: sets bits WGM00 and WGM01, which (when combined with WGM02 from TCCR0B below) enables Fast PWM mode
+    TCCR0A = (2 << COM0A0) | (2 << COM0B0) | (3 << WGM00);
+   
+//    Control Register B for Timer/Counter-0 (Timer/Counter-0 is configured using two registers: A and B)
+//    TCCR0B is 8 bits: [FOC0A:FOC0B:unused:unused:WGM02:CS02:CS01:CS00]
+//    0<<WGM02: bit WGM02 remains clear, which (when combined with WGM00 and WGM01 from TCCR0A above) enables Fast PWM mode
+//    1<<CS00: sets bits CS01 (leaving CS01 and CS02 clear), which tells Timer/Counter-0 to not use a prescalar
+    TCCR0B = (0 << WGM02) | (1 << CS01);//prescaler to 8
+
+//    Control Register for Timer/Counter-1 (Timer/Counter-1 is configured with just one register: this one)
+//    TCCR1 is 8 bits: [CTC1:PWM1A:COM1A1:COM1A0:CS13:CS12:CS11:CS10]
+//    0<<PWM1A: bit PWM1A remains clear, which prevents Timer/Counter-1 from using pin OC1A (which is shared with OC0B)
+//    0<<COM1A0: bits COM1A0 and COM1A1 remain clear, which also prevents Timer/Counter-1 from using pin OC1A (see PWM1A above)
+//    1<<CS10: sets bit CS11 which tells Timer/Counter-1  to not use a prescalar
+    TCCR1 = (0 << PWM1A) | (0 << COM1A0) | (1 << CS10);
+   
+//    General Control Register for Timer/Counter-1 (this is for Timer/Counter-1 and is a poorly named register)
+//    GTCCR is 8 bits: [TSM:PWM1B:COM1B1:COM1B0:FOC1B:FOC1A:PSR1:PSR0]
+//    1<<PWM1B: sets bit PWM1B which enables the use of OC1B (since we disabled using OC1A in TCCR1)
+//    2<<COM1B0: sets bit COM1B1 and leaves COM1B0 clear, which (when in PWM mode) clears OC1B on compare-match, and sets at BOTTOM
+    GTCCR = (1 << PWM1B) | (2 << COM1B0);
+  
+    TinyWireS.begin(I2C_ADDRESS_ATTINY85);
+    TinyWireS.onReceive(receiveEvent);
+    //TinyWireS.onRequest(requestEvent);
+    
+    analogWrite(PUMP_PIN, 0);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void loop() {
+    if(PINB & 0x10){
+        PORTB |= (1 << PB3);
+    }
+    else{
+        PORTB &= (~(1 << PB3));
+    }
+
+    //PORTB &= (~( 1 <<IB_MCP));
+    //PORTB |= (1 << IB_MCP); //IB is set high
+    //analogWrite(PUMP_PIN, pwm);
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
